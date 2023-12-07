5915adfe617454b62e146027555a345706ed688c
lpxd <tosiaput@gmail.com>
2023-12-07 17:15:11 +0100

    Create idk.java
    just trying

---
+++ idk.java
@@ -0,0 +1,10 @@
+class Project{
+    public Project(){
+        System.out.println("Hi");
+        
+    }
+    public static void main(String[] args){
+        Project puppy = new Project();
+        
+    }
+}
